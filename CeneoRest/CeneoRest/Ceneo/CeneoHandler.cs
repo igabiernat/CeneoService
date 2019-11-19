7f637d1b306228394217583f6b9299a00930dea7
Qtyb <pkutyba@zoho.eu>
2019-11-19 20:43:47 +0100

    add: downloading ceneo page

---
+++ CeneoRest/CeneoRest/Ceneo/CeneoHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace CeneoRest.Ceneo
+{
+    public static class CeneoHandler
+    {
+        public static async Task<IActionResult> HandleOfferRequest(string uri)
+        {
+            try
+            {
+                Log.Information($"Request for uri {uri} started");
+                var startTime = DateTime.Now;
+                var page = await ScrapPage(uri);
+                //TODO PARSE PAGE
+                //TODO SEARCH FOR DATA AND ASSIGN IT TO result
+                
+                var result = "RESULT";
+                var totalTime = DateTime.Now - startTime;
+                Log.Information($"Request handled in {totalTime.TotalSeconds} seconds");
+                return new JsonResult(result)
+                {
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e.Message);
+                return new JsonResult($"Fatal error: {e.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+            
+            
+        }
+        private static async Task<string> ScrapPage(string uri)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(uri);
+            var pageContents = await response.Content.ReadAsStringAsync();
+
+            return pageContents;
+        }
+    }
+}
--- CeneoRest/CeneoRest/Controllers/CeneoController.cs
+++ CeneoRest/CeneoRest/Controllers/CeneoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CeneoRest.Ceneo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,14 @@ namespace CeneoRest.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "ceneo", "value" };
+        }
+
+        [HttpPost("offer")]
+        public async Task<IActionResult> Offer()
+        {
+            var item = "telefon";
+            var result = await CeneoHandler.HandleOfferRequest($"https://www.ceneo.pl/szukaj-{item}/".ToLower());
+            return StatusCode( 200,result);
         }
     }
 }
