1f5205b1084878371d3089802e7bd6400c3f0e15
Qtyb <pkutyba@zoho.eu>
2019-11-24 13:47:44 +0100

    add: creating models

---
+++ CeneoRest/CeneoRest/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeneoRest.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public int num { get; set; }
+        public int min_price { get; set; }
+        public int max_price { get; set; }
+        public int min_reputation { get; set; }
+    }
+}
---
+++ CeneoRest/CeneoRest/Models/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CeneoRest.Models
+{
+    public class SearchResult
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal ShippingCost { get; set; }
+        public string Link { get; set; }
+        public string Info { get; set; }
+    }
+}
