a77f9669eb437742f9f2cd96fed6059fbfb8546e
igabiernat <49348432+igabiernat@users.noreply.github.com>
2019-11-29 20:36:32 +0100

    ScrapPage fixed

--- CeneoRest/CeneoRest/Ceneo/CeneoHandler.cs
+++ CeneoRest/CeneoRest/Ceneo/CeneoHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.NetworkInformation;
+using System.Net;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using CeneoRest.Models;
@@ -10,6 +12,8 @@ using HtmlAgilityPack;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
 
 namespace CeneoRest.Ceneo
 {
@@ -23,7 +27,9 @@ namespace CeneoRest.Ceneo
             //PARALLEL CZYLI WIELOWĄTKOWO - "na raz wyślemy zapytania o wszystkie produkty, a nie będziemy czekać po kolei na każdy." Jak nie zadziala to zrobimy normalnie.
             Parallel.ForEach(products, async product =>
             {
-                var uri = $"https://www.ceneo.pl/szukaj-{product.name.Trim()}/";
+                var uri = $"http://www.ceneo.pl/szukaj-{product.name.Trim()}";
+                //var uri = $"https://www.amazon.com/s?k=samsung+galaxy+s9&ref=nb_sb_noss_2/";
+                //var uri = $"http://www.ceneo.pl/Telefony_i_akcesoria;szukaj-samsung+galaxy+s9";
                 var pageContents = await ScrapPage(uri);
                 HtmlDocument pageDocument = new HtmlDocument();
                 pageDocument.LoadHtml(pageContents);
@@ -68,11 +74,9 @@ namespace CeneoRest.Ceneo
         }
         private async Task<string> ScrapPage(string uri)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(uri);
-             var pageContents = await response.Content.ReadAsStringAsync();
-
-            return pageContents;
+            var httpClient = new HttpClient();
+            var pageContents = httpClient.GetStringAsync(uri);
+            return await pageContents;
         }
 
         private SearchResult CalculateBestSearchResult(HtmlDocument pageDocument)
