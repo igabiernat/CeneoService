794bf541c9e236742763dedbc232204b579781d4
igabiernat <49348432+igabiernat@users.noreply.github.com>
2019-12-01 15:58:49 +0100

    extracting data from files

--- CeneoRest/CeneoRest/Ceneo/CeneoHandler.cs
+++ CeneoRest/CeneoRest/Ceneo/CeneoHandler.cs
@@ -28,7 +28,8 @@ namespace CeneoRest.Ceneo
                 var uri = $"https://www.ceneo.pl/;szukaj-{product.name.Trim()}";
                 var pageContents = await ScrapPage(uri);
                 HtmlDocument pageDocument = new HtmlDocument();
-                pageDocument.LoadHtml(pageContents);
+                //pageDocument.LoadHtml(pageContents);
+                pageDocument.Load("CeneoHTML.html");    //na razie z pliku
                 var result = CalculateBestSearchResult(pageDocument);
                 searchResults.Add(result);
                 Log.Information("FOREACH STOP");
@@ -52,16 +53,40 @@ namespace CeneoRest.Ceneo
             //throw new NotImplementedException();
             //TODO
 
-            var ProductList = pageDocument.DocumentNode.Descendants("div")
-               .Where(node => node.GetAttributeValue("class", "")
-               .Equals("category-list-body js_category-list-body js_search-results")).ToList();
-            //var pickedProduct = pageDocument.DocumentNode.SelectSingleNode("//div[@class = '\"category-list-body js_category-list-body js_search-results\"']");
-            //var pickedProduct = pageDocument.DocumentNode.SelectSingleNode("//div[@class = 'category-list-body js_category-list-body js_search-results']//strong[@class='cat-prod-row-name']/a");
-            //var productId = pickedProduct.Attributes["href"].Value;
-            //var picked = pageDocument.DocumentNode.Descendants("strong")
-            //    .Where(node => node.GetAttributeValue("class","")
-            //    .Equals("\"cat-prod-row-name\"")).ToList();
+            var productsList = pageDocument.DocumentNode.Descendants("strong") //wczytywanie listy produktów    
+                .Where(node => node.GetAttributeValue("class","")
+                .Equals("cat-prod-row-name")).ToList();
 
+            var productInfo = productsList[1].SelectSingleNode("a");   //wybranie pierwszego produktu (który nie jest sponsorowany) //TODO - wybieranie produktu który ma powyżej 5 sklepów
+            
+            var id = productInfo.GetAttributeValue("href", ""); //pobieranie id produktu //TODO - generowanie url, który przeniesie do wybranego produktu
+
+            HtmlDocument chosenProduct = new HtmlDocument();
+            chosenProduct.Load("productHTML.html"); //na razie z pliku bo inaczej nie działa :)
+
+            var shopsList = chosenProduct.DocumentNode.Descendants("tr")    //wczytywanie listy sklepów oferujących sprzedaż wybranego produktu
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("product-offer clickable-offer js_offer-container-click  js_product-offer")).ToList();
+            var shopChosen = shopsList[1];  //wybór sklepu //TODO - wybór najlepszego sklepu
+
+            var sellersName = shopChosen.GetAttributeValue("data-shopurl", ""); //pobranie nazwy sprzedającego
+
+            var rating = shopChosen.Descendants("span") //pobranie ilości gwiazdek
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("screen-reader-text")).First().InnerText;
+
+            var numberOfRatings = shopChosen.Descendants("span")    //pobranie ilości opinii
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("dotted-link js_mini-shop-info js_no-conv")).First().InnerText;
+            
+            var ship = shopChosen.Descendants("div")    //pobranie informacji o wysyłce //TODO
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("product-delivery-info js_deliveryInfo")).First().InnerText;
+
+            var price = shopChosen.Descendants("span")  //pobranie ceny produktu
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("price-format nowrap")).First().FirstChild.InnerText;
+          
             var result = new SearchResult { Info = "Test", Price = 9.5M, ShippingCost = 1M, Name = "Testowy", Link = "https://www.ceneo.pl/", SellersName = "RTV EURO AGD"};
             usedSellers.Add(result.SellersName);
             return result;
