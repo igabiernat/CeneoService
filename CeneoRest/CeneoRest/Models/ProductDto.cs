e86b110662776f98b09ee2a8dfa651a32f46555e
Qtyb <pkutyba@zoho.eu>
2019-12-03 16:01:43 +0100

    add: rest ready for testing spa integration

--- CeneoRest/CeneoRest/Models/ProductDto.cs
+++ CeneoRest/CeneoRest/Models/ProductDto.cs
@@ -8,9 +8,9 @@ namespace CeneoRest.Models
     public class ProductDto
     {
         public string name { get; set; }
-        public int num { get; set; }
-        public int min_price { get; set; }
-        public int max_price { get; set; }
-        public int min_reputation { get; set; }
+        public int? num { get; set; }
+        public Decimal? min_price { get; set; }
+        public Decimal? max_price { get; set; }
+        public Decimal? min_reputation { get; set; }
     }
 }
